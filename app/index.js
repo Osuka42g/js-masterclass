f29dba1fab0ae9cb713eca41274ea33963e22aee
Osuka42g <osuka42@gmail.com>
2018-09-24 17:42:28 -0500

    Create server

---
+++ app/index.js
@@ -0,0 +1,30 @@
+/*
+ * Primary file for the API
+ * 
+ */
+
+// Dependencies
+var http = require("http");
+var url = require("url");
+
+// The server should respond to all requests with a string
+var server = http.createServer(function(req, res) {
+
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/\/+|\/+$/g, "");
+
+    // Send the response
+    res.end("Hello world\n");
+
+    // Log the request path
+    console.log("Request received on path: " + trimmedPath)
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, function() {
+    console.log("The server is listening on port 3000 now");
+});
