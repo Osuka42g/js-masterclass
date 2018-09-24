5796a5a27f04f254b4bdc8f5c77f6413b3f5da65
Osuka42g <osuka42@gmail.com>
2018-09-24 18:37:23 -0500

    Parse payload

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var http = require("http");
 var url = require("url");
+var stringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -26,11 +27,24 @@ var server = http.createServer(function(req, res) {
     // Get the headers as an object
     var headers = req.headers;
 
-    // Send the response
-    res.end("Hello world\n");
+    // Get the payload, if any
+    var decoder = new StringDecoder("utf-8");
+    var buffer = "";
+    
+    req.on("data", function(data) {
+         buffer += decoder.write(data);
+    });
 
-    // Log the request path
-    console.log("Request received with these headers", headers);
+    req.on("end", function() {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end("Hello world\n");
+    
+        // Log the request path
+        console.log("Request received with this payload", buffer);
+    });
+
 });
 
 // Start the server, and have it listen on port 3000
