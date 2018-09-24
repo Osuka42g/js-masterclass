befce16c5e0487c3864902a117d0b86042dd8d6e
Osuka42g <osuka42@gmail.com>
2018-09-24 18:07:55 -0500

    Parse query string

--- app/index.js
+++ app/index.js
@@ -17,6 +17,9 @@ var server = http.createServer(function(req, res) {
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/\/+|\/+$/g, "");
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
     // Get the HTTP Method
     var method = req.method.toLowerCase();
 
@@ -24,7 +27,7 @@ var server = http.createServer(function(req, res) {
     res.end("Hello world\n");
 
     // Log the request path
-    console.log("Request received on path: " + trimmedPath + " with method: " + method);
+    console.log("Request received on path: " + trimmedPath + " with method: " + method + " with these query string parameters ", queryStringObject);
 });
 
 // Start the server, and have it listen on port 3000
